65a0cdc8581b19f5e8fef6cad2a73c5dab86360f
Elina <elinakolod@gmail.com>
2018-12-21 12:04:17 +0200

    configue project attributes

---
+++ chef/site-cookbooks/app-attributes/attributes/default.rb
@@ -0,0 +1,4 @@
+# Project -------------------------------------------------------
+
+override['project']['name'] = 'chef_app'
+override['project']['repository'] = 'git@github.com:elinakolod/chef_app.git'
