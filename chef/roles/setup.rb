ae66c7d4e2e3b1c792140666090dd34995ba318b
Elina <elinakolod@gmail.com>
2018-12-21 12:25:16 +0200

    configue sudo privilege

---
+++ chef/roles/setup.rb
@@ -0,0 +1,7 @@
+name 'setup'
+description 'Basic setup'
+
+# Run default recipes of these cookbooks one-by-one
+run_list 'recipe[app-hostname]',
+         'recipe[app-users]',
+	 'recipe[app-sudo]'
---
+++ chef/site-cookbooks/app-sudo/metadata.rb
@@ -0,0 +1,4 @@
+name    'app-sudo'
+version '0.1.0'
+
+depends 'sudo'
